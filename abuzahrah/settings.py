184fe0468eaa908768d5561c4b5dd9bd80f6c3bd
abuzahrah75 <abuzahrah75@gmail.com>
2022-05-12 03:21:54 +0000

    fix static issue

--- abuzahrah/settings.py
+++ abuzahrah/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "theme/static"),
+]
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
